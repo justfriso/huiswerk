f1c7bf6270f5e0e3285620dd9ff35578b0e2fb37
justfriso <huizengafriso@gmail.com>
2023-05-15 12:03:22 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Print een verhaaltje over mijzelf
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mijn naam is Friso Huizenga.");
+        System.out.println("Ik ben 22 jaar oud.");
+    }
+}
