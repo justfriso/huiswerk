929041ea9077c76bab4c1b81fcc71191be46b794
justfriso <huizengafriso@gmail.com>
2023-05-15 13:23:20 +0200

    introductie toegevoegd en wat aanpassingen gemaakt.

--- src/introductie.java
+++ src/introductie.java
@@ -1,8 +1,12 @@
 public class introductie {
     public introductie(String naam) {
-        System.out.print("hoi ");
+        System.out.print("mijn naam is ");
         System.out.print(naam);
-        System.out.print("!");
+        System.out.print(".");
+        System.out.println("Ik woon in Lelystad.");
+        System.out.println("Ik ben 22 jaar oud.");
+        System.out.println("Ik houd erg van alles wat met sporten en/of buiten zijn te maken heeft.");
+        System.out.println("Ook houd ik erg van dieren.");
 
     }
 }
