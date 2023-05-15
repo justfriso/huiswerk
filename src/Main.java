fecc73c84717b1859290408029f6c248ece61bb7
justfriso <huizengafriso@gmail.com>
2023-05-15 13:17:48 +0200

    introductie toegevoegd en wat aanpassingen gemaakt.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mijn naam is Friso Huizenga.");
-        System.out.println("Ik woon in Lelystad.");
-        System.out.println("Ik ben 22 jaar oud.");
-        System.out.println("Ik houd erg van sporten.");
-        System.out.println("Ook ben ik graag bezig met dieren.");
+        introductie tijdelijk = new introductie("friso");
     }
 }
---
+++ src/introductie.java
@@ -0,0 +1,8 @@
+public class introductie {
+    public introductie(String naam) {
+        System.out.print("hoi ");
+        System.out.print(naam);
+        System.out.print("!");
+
+    }
+}
