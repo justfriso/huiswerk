116f6a922e43e71637b1a060b333c5a689200fc1
justfriso <huizengafriso@gmail.com>
2023-05-15 12:12:14 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Mijn naam is Friso Huizenga.");
+        System.out.println("Ik woon in Lelystad.");
         System.out.println("Ik ben 22 jaar oud.");
         System.out.println("Ik houd erg van sporten.");
+        System.out.println("Ook ben ik graag bezig met dieren.");
     }
 }
